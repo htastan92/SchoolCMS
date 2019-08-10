280f4bd0cb472af09227cfe5ce6f3e2060cbf680
Muhsin ARSLAN <muhsinarslan2019@gmail.com>
2019-08-10 19:30:54 +0300

    Update
    * Slug added for content entity.

--- Entities/Content.cs
+++ Entities/Content.cs
@@ -7,6 +7,7 @@ namespace Entities
         public int Id { get; set; }
 
         public string Name { get; set; }
+        public string Slug { get; set; }
         public string Description { get; set; }
         public string EditorContent { get; set; }
 
