fda79d8d517f45ad2e578160b764cd7984070199
Huseyın Tastan <htastan3458@gmail.com>
2019-08-11 17:32:57 +0300

    CampusViewModels

--- Admin/Models/CampusListViewModel.cs
+++ Admin/Models/CampusListViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace Admin.Models
+﻿using System.Collections.Generic;
+using Entities;
+
+namespace Admin.Models
 {
     public class CampusListViewModel
     {
+        public IList<Campus> Campuses { get; set; }
     }
 }
--- Admin/Models/CampusNewViewModel.cs
+++ Admin/Models/CampusNewViewModel.cs
@@ -7,5 +7,15 @@ namespace Admin.Models
 {
     public class CampusNewViewModel
     {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string EditorContent { get; set; }
+        public int StatusId { get; set; }
+        public string ImageUrl { get; set; }
+        public string Address { get; set; }
+        public string Telephone { get; set; }
+        public string Fax { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
