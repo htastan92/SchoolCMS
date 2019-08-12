4409544cd89aa3464356abc8ec8fff5248ad3ed2
Huseyın Tastan <htastan3458@gmail.com>
2019-08-13 01:45:22 +0300

    update

--- Admin/Startup.cs
+++ Admin/Startup.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Globalization;
+using Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Service;
 
 namespace Admin
 {
@@ -30,6 +32,24 @@ namespace Admin
                 options.CheckConsentNeeded = context => false;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<SchoolContext,SchoolContext>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<ICampusService, CampusService>();
+            services.AddScoped<IDashboardService, DashboardService>();
+            services.AddScoped<IEventCategoryService, EventCategoryService>();
+            services.AddScoped<IFormService, FormService>();
+            services.AddScoped<IEventService, EventService>();
+            services.AddScoped<IImageService, ImageService>();
+            services.AddScoped<IMemberService, MemberService>();
+            services.AddScoped<IMenuService, MenuService>();
+            services.AddScoped<ISettingsService, SettingsService>();
+            services.AddScoped<IStaffService, StaffService>();
+            services.AddScoped<IStatusService, StatusService>();
+            services.AddScoped<INewsService, NewsService>();
+            services.AddScoped<IPageService, PageService>();
 
             services.AddSession(options =>
             {
