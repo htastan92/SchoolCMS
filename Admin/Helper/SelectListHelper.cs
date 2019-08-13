33c9c6ffc437ab297d3ab3db62ece4b7929fae50
Huseyın Tastan <htastan3458@gmail.com>
2019-08-13 03:42:45 +0300

    Campus and Event Views

---
+++ Admin/Helper/SelectListHelper.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Data;
+using Entities;
+
+namespace Admin.Helper
+{
+    public class SelectListHelper
+    {
+        public static SelectList StatusSelectList()
+        {
+            IEnumerable<Status> statuses;
+            using (SchoolContext db = new SchoolContext())
+            {
+                statuses = db.Statuses.ToList();
+            }
+
+            SelectList statuList = new SelectList(statuses, "Id", "Name");
+            return statuList;
+        }
+
+        public static SelectList EventCategorySelectList()
+        {
+            IEnumerable<EventCategory> eventCategories;
+            using (SchoolContext db = new SchoolContext())
+            {
+                eventCategories = db.EventCategories.ToList();
+            }
+
+            SelectList eventCategoryList = new SelectList(eventCategories, "Id", "Name");
+            return eventCategoryList;
+        }
+        public static SelectList newsCategorySelectList()
+        {
+            IEnumerable<NewsCategory> newsCategories;
+            using (SchoolContext db = new SchoolContext())
+            {
+                newsCategories = db.NewsCategories.ToList();
+            }
+
+            SelectList newsCategoryList = new SelectList(newsCategories, "Id", "Name");
+            return newsCategoryList;
+        }
+        public static SelectList campusSelectList()
+        {
+            IEnumerable<Campus> campuses;
+            using (SchoolContext db = new SchoolContext())
+            {
+                campuses = db.Campuses.ToList();
+            }
+
+            SelectList campuSelectList = new SelectList(campuses, "Id", "Name");
+            return campuSelectList;
+        }
+    }
+}
